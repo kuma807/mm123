2f1363f549d37b4ebc6e4f138a8dc33c6191b569
kuma807 <kumakura013@gmail.com>
2021-01-29 17:42:54 +0900

    update score

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,12 @@
 import subprocess
+import time
 sum = 0
 for i in range(50):
+    start = time.time()
     res = str(subprocess.check_output(['./run.sh', str(i)]))
     res = res[10:]
     res = res.replace('\\n', '').replace('\'', '')
     sum += float(res)
+    end = time.time()
+    print(end - start)
 print(sum)
